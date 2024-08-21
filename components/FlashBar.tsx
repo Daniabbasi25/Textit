fa5b41fa51539ad0acb01d202455ef3423594f0c
Daniabbasi25 <danyal.munir@hysetech.net>
2024-08-21 19:43:38 +0500

    flash bar is done

--- components/FlashBar.tsx
+++ components/FlashBar.tsx
@@ -1,12 +1,14 @@
 import { fontFamilies, getFontSize } from "@/lib";
 import { FlashBarProps } from "@/lib/interfaces";
 import React, { useState, useEffect, type FC } from "react";
-import { Text, StyleSheet, Dimensions } from "react-native";
+import { Text, StyleSheet, Dimensions, View } from "react-native";
 import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedStyle,
 } from "react-native-reanimated";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Colors } from "@/theme";
 
 interface Props extends FlashBarProps {
   isVisible: boolean;
@@ -55,14 +57,33 @@ const FlashBar: FC<Props> = ({ type, message, isVisible, onHide }) => {
         return {};
     }
   };
+  const getColor = () => {
+    switch (type) {
+      case "success":
+        return "#3EAE4D";
 
+      case "danger":
+        return "#ED2828";
+
+      case "info":
+        return "#C5E6FF";
+      default:
+        return "#C5E6FF";
+    }
+  };
   return (
     <>
       {isVisibleInternal && (
-        <Animated.View
-          style={[styles.container, getMessageStyle(), animatedStyle]}
-        >
-          <Text style={styles.message}>{message}</Text>
+        <Animated.View style={[styles.container, animatedStyle]}>
+          <View style={[styles.left, getMessageStyle()]} />
+
+          <AntDesign name="checkcircle" size={24} color={getColor()} />
+          <View style={{ paddingVertical: 15 }}>
+            <Text style={styles.heading}>
+              {type === "danger" ? "Error" : type}
+            </Text>
+            <Text style={styles.message}>{message}</Text>
+          </View>
         </Animated.View>
       )}
     </>
@@ -75,13 +96,35 @@ const styles = StyleSheet.create({
     top: 80,
     left: 20,
     right: 20,
-    padding: 15,
     borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+    backgroundColor: Colors.AuthHeading,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    overflow: "hidden",
   },
   message: {
     color: `white`,
     fontFamily: fontFamilies.Popins.normal,
+    fontSize: getFontSize(10),
+  },
+  heading: {
+    fontFamily: fontFamilies.Popins.bold,
     fontSize: getFontSize(12),
+    color: `white`,
+    textTransform: "capitalize",
+  },
+  left: {
+    height: "100%",
+    width: "2%",
   },
 });
 
