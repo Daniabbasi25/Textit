b9b984ade3cefb440130e226858a517e338864a2
Daniabbasi25 <danyal.munir@hysetech.net>
2024-08-26 19:33:21 +0500

    created global tabbarheader

---
+++ components/TabBarHeader.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { FC, ReactNode } from "react";
+import { getWidth } from "@/lib";
+import ScreenHeading from "./ScreenHeading";
+
+interface Props {
+  leftComponent: ReactNode;
+  name: string;
+  rightComponent: ReactNode;
+}
+const TabBarHeader: FC<Props> = ({ leftComponent, name, rightComponent }) => {
+  return (
+    <View style={styles.container}>
+      {leftComponent}
+      <ScreenHeading text="Home" />
+      {rightComponent}
+    </View>
+  );
+};
+
+export default TabBarHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: getWidth(5),
+  },
+});
--- components/chat/container/ChatScreenHeader.tsx
+++ components/chat/container/ChatScreenHeader.tsx
@@ -1,29 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Icon from "../ui/Icon";
-import { getWidth } from "@/lib";
 import Avatar from "../ui/Avatar";
-import ScreenHeading from "../ui/ScreenHeading";
+import TabBarHeader from "@/components/TabBarHeader";
 
 const ChatScreenHeader = () => {
   return (
-    <View style={styles.container}>
-      {/* Search Icon */}
-      <Icon name="search" onpress={() => {}} />
-      <ScreenHeading text="Home" />
-      {/* Avater */}
-      <Avatar uri="https://avatar.iran.liara.run/username?username=Discuss react native" />
-    </View>
+    <TabBarHeader
+      leftComponent={<Icon name="search" onpress={() => {}} />}
+      name="Home"
+      rightComponent={
+        <Avatar uri="https://avatar.iran.liara.run/username?username=Discuss react native" />
+      }
+    />
   );
 };
 
 export default ChatScreenHeader;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: getWidth(5),
-  },
-});
